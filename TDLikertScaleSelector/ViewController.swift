835678af615b83a25ecc701835305e74d192970a
Paul Leo <paul@tapdigital.com>
2018-07-27 10:26:14 +0100

    Initial Commit

---
+++ TDLikertScaleSelector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDLikertScaleSelector
+//
+//  Created by Paul Leo on 27/07/2018.
+//  Copyright © 2018 tapdigital Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TDLikertScaleSelectorTests/TDLikertScaleSelectorTests.swift
@@ -0,0 +1,34 @@
+//
+//  TDLikertScaleSelectorTests.swift
+//  TDLikertScaleSelectorTests
+//
+//  Created by Paul Leo on 27/07/2018.
+//  Copyright © 2018 tapdigital Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import TDLikertScaleSelector
+
+class TDLikertScaleSelectorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
